679a466989c1eeca1dffc004ded05dd2b3222fed
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-07-13 16:42:33 -0500

    hourHand

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
   const now = new Date();
@@ -10,5 +11,8 @@ function setDate() {
   const minutes = now.getMinutes();
   const minutesDegree = (minutes / 60) * 360 + 90;
   minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+  const hoursDegree = (minutes / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 }
 setInterval(setDate, 1000);
