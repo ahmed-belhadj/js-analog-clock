ace23212f56769674ad27c017b5bcc1aff1710a6
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-07-13 16:57:14 -0500

    Corrected time

--- scripts.js
+++ scripts.js
@@ -4,15 +4,20 @@ const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
   const now = new Date();
+
   const seconds = now.getSeconds();
   const secondsDegree = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 
   const minutes = now.getMinutes();
-  const minutesDegree = (minutes / 60) * 360 + 90;
+  const minutesDegree = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
   minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
 
-  const hoursDegree = (minutes / 12) * 360 + 90;
+  const hour = now.getHours();
+  const hoursDegree = (hour / 12) * 360 + (minutes / 60) * 30 + 90;
   hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 }
+
 setInterval(setDate, 1000);
+
+setDate();
